8c3ac984332c539c8b5cf6a74a5b65d20db3de88
acer <acer@TELMAY>
2024-12-29 17:11:48 +0800

    TRIAL

--- depart1/urls.py
+++ depart1/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path("member/", views.index, name="index"),
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("treasurer/", views.treasurer, name="treasurer")  # Add this line
 ]
