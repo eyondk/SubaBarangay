2969d997982597903c82f0229f1375c3a9cfad69
jaded <christinejade.duran@ctu.edu.ph>
2024-12-31 04:22:27 +0800

    change file format for reusability

--- depart1/urls.py
+++ depart1/urls.py
@@ -4,14 +4,28 @@ from . import views
 urlpatterns = [
     path("member/", views.index, name="index"),
     path("", views.index, name="index"),
+
     path("treasurer/dashboard/", views.treasurer_dashboard_view, name="Tdashboard"),
     path("treasurer/residents/", views.treasurer_resident_view, name="Tresidents"),
     path("treasurer/logs/", views.treasurer_logs_view, name="Tlogs"),
-    path("treasurer/users/", views.treasurer_users_view, name="Tusers"),
-    #path("treasurer/resident/<int:resident_id>/", views.treasurer_view_resident, name="view_resident")  
+    path("treasurer/users/", views.treasurer_users_view, name="Tusers"),    
     path("treasurer/resident/view/details", views.treasurer_resident_details_view, name="Tresident_details"),
-    path("treasurer/resident/view/husband", views.treasurer_resident_husband_view, name="Tresident_husband"),
     path("treasurer/resident/view/children", views.treasurer_resident_children_view, name= "Tresident_children"),
-    path("treasurer/resident/view/parents", views.treasurer_resident_parents_view, name= "Tresident_parents")
+    path("treasurer/resident/view/parents", views.treasurer_resident_parents_view, name= "Tresident_parents"),
+    path("treasurer/resident/view/husband", views.treasurer_resident_husband_view, name="Tresident_husband"),
+
+    #path("treasurer/resident/<int:resident_id>/", views.treasurer_view_resident, name="view_resident")  
+    path("resident/view/details", views.resident_details_view, name="resident_details"),
+    path("resident/view/husband", views.resident_husband_view, name="resident_husband"),
+    path("resident/view/children", views.resident_children_view, name= "resident_children"),
+    path("resident/view/parents", views.resident_parents_view, name= "resident_parents"),
+
+    path("captain/dashboard/", views.captain_dashboard_view, name="Cdashboard"),
+    path("captain/resident/", views.captain_resident_view, name="Cresidents"),
+    path("captain/logs/", views.captain_logs_view, name="Clogs"),
+    path("captain/resident/view/details", views.captain_resident_details_view, name= "Cresident_details"),
+    path("captain/resident/view/children", views.captain_resident_children_view, name= "Cresident_children"),
+    path("captain/resident/view/parents", views.captain_resident_parents_view, name= "Cresident_parents"),
+    path("captain/resident/view/husband", views.captain_resident_husband_view, name="Cresident_husband")
 
 ]
--- depart1/views.py
+++ depart1/views.py
@@ -2,9 +2,32 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 
+
+
 def index(request):
     return render(request,"secretary/secretary.html")
 
+#----------------RESIDENT----------------
+
+def resident_details_view(request): #, resident_id):
+    # Fetch resident data by ID from PostgreSQL
+    # try:
+    #     resident = Resident.objects.get(id=resident_id)
+    # except Resident.DoesNotExist:
+    #     resident = None  # Handle non-existent resident gracefully
+    #return render(request, "treasurer/Tview_resident.html", {"resident": resident})
+    return render(request, "shared/resident_details.html")
+
+def resident_husband_view(request):
+    return render(request, "shared/resident_husband.html")
+
+def resident_children_view(request):
+    return render(request, "shared/resident_children.html")
+
+def resident_parents_view(request):
+    return render(request, "shared/resident_parents.html")
+
+#------------TREASURER--------
 def treasurer_dashboard_view(request):
     return render(request, "treasurer/Tdashboard.html")
 
@@ -17,21 +40,39 @@ def treasurer_logs_view(request):
 def treasurer_users_view(request):
     return render(request, "treasurer/Tusers.html")
 
-def treasurer_resident_details_view(request): #, resident_id):
-    # Fetch resident data by ID from PostgreSQL
-    # try:
-    #     resident = Resident.objects.get(id=resident_id)
-    # except Resident.DoesNotExist:
-    #     resident = None  # Handle non-existent resident gracefully
-    #return render(request, "treasurer/Tview_resident.html", {"resident": resident})
+def treasurer_resident_details_view(request):
     return render(request, "treasurer/Tresident_details.html")
-
-def treasurer_resident_husband_view(request):
-    return render(request, "treasurer/Tresident_husband.html")
 
 def treasurer_resident_children_view(request):
     return render(request, "treasurer/Tresident_children.html")
 
 def treasurer_resident_parents_view(request):
     return render(request, "treasurer/Tresident_parents.html")
+
+def treasurer_resident_husband_view(request):
+    return render(request, "treasurer/Tresident_husband.html")
+
+# ==========CAPTAIN==============
+def captain_dashboard_view(request):
+    return render(request, "captain/Cdashboard.html")
+
+def captain_resident_view(request):
+    return render(request, "captain/Cresident.html")
+
+def captain_logs_view(request):
+    return render(request, "captain/Clogs.html")
+
+def captain_resident_details_view(request):
+    return render(request, "captain/Cresident_details.html")
+
+def captain_resident_children_view(request):
+    return render(request, "captain/Cresident_children.html")
+
+def captain_resident_parents_view(request):
+    return render(request, "captain/Cresident_parents.html")
+
+def captain_resident_husband_view(request):
+    return render(request, "captain/Cresident_husband.html")
+
+
 
