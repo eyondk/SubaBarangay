0869fc279cd611fa548604021a0fbe150285eb2f
jaded <christinejade.duran@ctu.edu.ph>
2024-12-30 17:46:47 +0800

    Done: View  All Resident's Details

--- depart1/urls.py
+++ depart1/urls.py
@@ -7,6 +7,11 @@ urlpatterns = [
     path("treasurer/dashboard/", views.treasurer_dashboard_view, name="Tdashboard"),
     path("treasurer/residents/", views.treasurer_resident_view, name="Tresidents"),
     path("treasurer/logs/", views.treasurer_logs_view, name="Tlogs"),
-    path("treasurer/users/", views.treasurer_users_view, name="Tusers")
+    path("treasurer/users/", views.treasurer_users_view, name="Tusers"),
+    #path("treasurer/resident/<int:resident_id>/", views.treasurer_view_resident, name="view_resident")  
+    path("treasurer/resident/view/details", views.treasurer_resident_details_view, name="Tresident_details"),
+    path("treasurer/resident/view/husband", views.treasurer_resident_husband_view, name="Tresident_husband"),
+    path("treasurer/resident/view/children", views.treasurer_resident_children_view, name= "Tresident_children"),
+    path("treasurer/resident/view/parents", views.treasurer_resident_parents_view, name= "Tresident_parents")
 
 ]
--- depart1/views.py
+++ depart1/views.py
@@ -16,3 +16,22 @@ def treasurer_logs_view(request):
 
 def treasurer_users_view(request):
     return render(request, "treasurer/Tusers.html")
+
+def treasurer_resident_details_view(request): #, resident_id):
+    # Fetch resident data by ID from PostgreSQL
+    # try:
+    #     resident = Resident.objects.get(id=resident_id)
+    # except Resident.DoesNotExist:
+    #     resident = None  # Handle non-existent resident gracefully
+    #return render(request, "treasurer/Tview_resident.html", {"resident": resident})
+    return render(request, "treasurer/Tresident_details.html")
+
+def treasurer_resident_husband_view(request):
+    return render(request, "treasurer/Tresident_husband.html")
+
+def treasurer_resident_children_view(request):
+    return render(request, "treasurer/Tresident_children.html")
+
+def treasurer_resident_parents_view(request):
+    return render(request, "treasurer/Tresident_parents.html")
+
